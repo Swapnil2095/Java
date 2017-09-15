892308ff8db01d9433a2db48bfc5d9997eefc8a8
Swapnil Gaikwad <swapnilgaikwad7291@gmail.com>
2017-09-16 00:55:22 +0530

    16/09/2017

---
+++ 2.Input & Output/Ways to read input from console/TestConsole.java
@@ -0,0 +1,13 @@
+// Java program to demonstrate working of System.console()
+// Note that this program does not work on IDEs as 
+// System.console() may require console
+public class TestConsole 
+{
+	public static void main(String[] args) 
+	{	 
+		// Using Console to input data from user
+		String name = System.console().readLine();
+		
+		System.out.println(name);
+	}
+}
